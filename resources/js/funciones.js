a0a09b2e8b248ff113142933fee84998f7db27f6
unknown <juliana.1701513897@ucaldas.edu.co>
2021-04-11 18:24:08 -0500

    added README.md

---
+++ resources/js/funciones.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*function validar(document){
+    let nombre = document.getElementById("nombre").value; 
+    let names = document.getElementsByName("nombres").value;
+    console.log(nombre);
+    console.log("jdkhfsd"); 
+    console.log(names);
+
+}*/
+
+document.addEventListener('click', async event => {
+
+    let nombre = document.getElementById("nombre").value; 
+    let names = document.getElementsByName("nombres").value;
+    console.log(nombre);
+    console.log("jdkhfsd"); 
+    console.log(names);
+
+});
+
